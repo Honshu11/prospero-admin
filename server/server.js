9f6ef3c9a64045f60322fb3fa4f3f8929df9c9ad
Honshu11 <egonza67@yahoo.com>
2022-08-01 12:38:16 -0700

    auto commit

--- server/server.js
+++ server/server.js
@@ -7,8 +7,15 @@ server.listen(80);
 console.log("started server on port 80");
 
 function handleRequest(request, response){
-    console.log("Hello world");
-    response.writeHead(200);
-    response.write("<h1>Hello World</h1>");
-    response.end();
+    
+    if(request.url == "/" && request.method == "GET"){
+        console.log("Hello world");
+        response.writeHead(200);
+        response.write("<h1>Hello World</h1>");
+        response.end();
+    } else {
+        response.write("404 file not found");
+        response.writeHead(404);
+        response.end();
+    }
 }
