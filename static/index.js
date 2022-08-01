733c4759a81f14f13184d28be7721ad8bca5255c
Honshu11 <egonza67@yahoo.com>
2022-08-01 13:19:35 -0700

    auto commit

--- static/index.js
+++ static/index.js
@@ -7,8 +7,16 @@ function createServer(){
 
     }
 
+    var data = {};
+
     fetch("https://143.198.138.219/api/server", {
         method: "POST",
+        body: JSON.stringify(data)
     })
 }
 
+function registerStaticEventHandlers(){
+    document.querySelector('form[name="new-server"]'); //name equals attribute value
+}
+
+registerStaticEventHandlers();
