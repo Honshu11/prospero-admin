e7c10885d0af1deaa57abb0816d955cfdd953cfc
Honshu11 <egonza67@yahoo.com>
2022-08-05 13:06:35 -0700

    add promise

--- server/server-express.js
+++ server/server-express.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+const db = (new mongodb.MongoClient(process.env.DB_STRING)).db('admin');
 
 app.get('/', function(request, response){
     response.send("Hello World");
@@ -7,6 +8,15 @@ app.get('/', function(request, response){
 
 app.get('/server/:id', function(request, response){
     response.send("Server " + request.params.id);
+})
+
+app.post('/api/servers', function(request, response){ //promise
+    db.collection('servers').insertOne({name: 'test server'}).then(function(serverData){
+        response.status(201); //returns connection and addition to something
+        response.send(serverData);
+    }).catch(function(){
+        response.status(500); //
+    });
 })
 
 app.listen(80);
