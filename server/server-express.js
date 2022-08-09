a26f2edc879b9d3a41df8be4bf4f8c3017a66f28
Honshu11 <egonza67@yahoo.com>
2022-08-09 12:57:59 -0700

    add authentication

--- server/server-express.js
+++ server/server-express.js
@@ -56,7 +56,7 @@ app.get('/api/servers', function(request, response){
 app.get('/api/droplets', function(request, response){
     fetch('https://api.digitalocean.com/v2/droplets', {
         headers: {
-            'Authorization': 'Bearer ' + process.env.DIGITALOCEAN_TOKEN;
+            'Authorization': 'Bearer ' + process.env.DIGITALOCEAN_TOKEN
         }
     }).then(function(response){
         if(response.ok){
