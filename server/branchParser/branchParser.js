706d07c35afe0927fa2596eae8be20a35c832c18
Honshu11 <egonza67@yahoo.com>
2022-09-06 13:20:31 -0700

    add hack to branchparser

--- server/branchParser/branchParser.js
+++ server/branchParser/branchParser.js
@@ -1,7 +1,7 @@
 
 function processBranchList(payload){
     var data = payload.toString();
-    console.log(data);
+    console.log('---\n' + data + '\n---');
     var branchEntries = data.split('\n');
     branchEntries = branchEntries.slice(0, branchEntries.length - 1);
     console.log('branchEntries: ', branchEntries);
