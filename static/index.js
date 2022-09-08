37377b81b9253477b6eb952e9fc660ba03d081c5
Honshu11 <egonza67@yahoo.com>
2022-09-08 11:51:43 -0700

    add console log in updateBranchDropDown

--- static/index.js
+++ static/index.js
@@ -97,6 +97,7 @@ function updateBranchDropDown(branches){
         var option = document.createElement('option');
         option.textContent = branch;
         dropdown.appendChild(option);
+        console.log(form, option, dropdown);
     })
 }
 
